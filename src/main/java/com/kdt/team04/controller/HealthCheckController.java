ae07e00add0403f24fda94cb3ab78299802ebc6b
HYEBPARK <hyeqls3742@gmail.com>
2022-07-19 14:17:48 +0900

    [DK-7] feat: 스웨거 설정

--- src/main/java/com/kdt/team04/controller/HealthCheckController.java
+++ src/main/java/com/kdt/team04/controller/HealthCheckController.java
@@ -3,9 +3,14 @@ package com.kdt.team04.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "health check api")
 @RestController
 public class HealthCheckController {
 
+	@Operation(summary = "건강 여부 판단", description = "실행이 잘 되는지 건강을 체크해봅시다.")
 	@GetMapping("/health")
 	public String check() {
 		return "health!!!";
