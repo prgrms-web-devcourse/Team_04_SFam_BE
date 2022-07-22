8b57013353985ac25ac064a343df7c8f48fc6c00
pjh612 <pjh_jn@naver.com>
2022-07-22 16:54:06 +0900

    [DK-114] feat:  auditing 추가

---
+++ src/main/java/com/kdt/team04/common/config/JpaAuditConfig.java
@@ -0,0 +1,38 @@
+package com.kdt.team04.common.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.kdt.team04.common.security.jwt.JwtAuthentication;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+
+	@Bean
+	public AuditorAware<String> autAuditorProvider() {
+		return () -> {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+			if (authentication == null || !authentication.isAuthenticated() || isAnonymous(authentication)) {
+				return Optional.empty();
+			}
+
+			JwtAuthentication user = (JwtAuthentication)authentication.getPrincipal();
+
+			return Optional.ofNullable(user.id().toString());
+		};
+	}
+
+	private boolean isAnonymous(Authentication authentication) {
+		return authentication instanceof AnonymousAuthenticationToken;
+	}
+
+}
