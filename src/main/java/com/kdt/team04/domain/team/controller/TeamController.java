9df9a346ac53f3a58e4f9d16009c11c781ca6e0a
HYEBPARK <hyeqls3742@gmail.com>
2022-07-23 23:24:59 +0900

    [DK-106] fix: 디버깅 편리를 위한 수정

--- src/main/java/com/kdt/team04/domain/team/controller/TeamController.java
+++ src/main/java/com/kdt/team04/domain/team/controller/TeamController.java
@@ -32,15 +32,18 @@ public class TeamController {
 	@PostMapping
 	public ApiResponse<TeamResponse> create(@AuthenticationPrincipal JwtAuthentication jwtAuthentication,
 		@RequestBody TeamRequest.CreateRequest request) {
-		return new ApiResponse<>(
-			teamService.create(jwtAuthentication.id(), request.name(), request.sportsCategory(),
-				request.description()));
+		TeamResponse team = teamService.create(jwtAuthentication.id(), request.name(), request.sportsCategory(),
+			request.description());
+
+		return new ApiResponse<>(team);
 	}
 
 	@Operation(summary = "팀 프로필 조회", description = "해당 id의 팀 프로필을 조회할 수 있습니다.")
 	@GetMapping("/{id}")
 	public ApiResponse<TeamResponse> getById(@PathVariable Long id) {
-		return new ApiResponse<>(teamService.findById(id));
+		TeamResponse team = teamService.findById(id);
+
+		return new ApiResponse<>(team);
 	}
 
 }
