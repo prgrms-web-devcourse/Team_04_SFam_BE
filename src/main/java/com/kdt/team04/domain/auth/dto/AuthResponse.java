5bbba07a116fbf99bd129bc3f9a169188ac3d481
pjh612 <pjh_jn@naver.com>
2022-07-23 16:57:46 +0900

    [DK-48] feat:  SignInResponse JsonIgnore 추가

--- src/main/java/com/kdt/team04/domain/auth/dto/AuthResponse.java
+++ src/main/java/com/kdt/team04/domain/auth/dto/AuthResponse.java
@@ -1,13 +1,17 @@
 package com.kdt.team04.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kdt.team04.common.security.jwt.JwtAuthenticationToken;
 
 public record AuthResponse() {
 	public record SignInResponse(
 		Long id,
 		String username,
+		@JsonIgnore
 		TokenDto accessToken,
+		@JsonIgnore
 		TokenDto refreshToken,
+		@JsonIgnore
 		JwtAuthenticationToken jwtAuthenticationToken
 	) {
 	}
