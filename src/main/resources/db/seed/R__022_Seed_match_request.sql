1f27fa4533bae721874a7e972170282a50be688e
midasworld <midas117@knou.ac.kr>
2022-07-28 14:17:09 +0900

    [DK-198] setting: 시드 파일 수정

--- src/main/resources/db/seed/R__022_Seed_match_request.sql
+++ src/main/resources/db/seed/R__022_Seed_match_request.sql
@@ -1,6 +1,6 @@
-INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(1, 1, 'ㅇㅇ', 5, 5, 'APPROVED');
-INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(2, 2, 'ㄱㄱ', 5, 5, 'APPROVED');
-INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(3, 3, '축구 ㄱㄱ', 5, 5, 'APPROVED');
+INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(1, 1, 'ㅇㅇ', 4, 5, 'APPROVED');
+INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(2, 2, 'ㄱㄱ', 4, 5, 'APPROVED');
+INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(3, 3, '축구 ㄱㄱ', 4, 5, 'APPROVED');
 INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(4, 5, 'ㄹㅇ ㅋㅋ', 3, null, 'WAITING');
-INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(5, 5, 'ㅇㅇ', 4, null, 'WAITING');
-INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(6, 5, 'ㅎㅇ', 5, null, 'APPROVED');
+INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(5, 5, 'ㅇㅇ', 2, null, 'WAITING');
+INSERT IGNORE INTO match_request(id, match_id, content, user_id, team_id, status) VALUES(6, 5, 'ㅎㅇ', 4, null, 'APPROVED');
