551521bbede52c884ac0ecf28f3219804c7280df
HYEBPARK <hyeqls3742@gmail.com>
2022-07-29 14:49:45 +0900

    fix : 불필요한 파일 삭제

--- src/main/java/com/kdt/team04/domain/match/review/repository/MatchReviewRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.kdt.team04.domain.match.review.repository;
-
-import com.kdt.team04.domain.matches.review.dto.MatchReviewResponse;
-
-public interface MatchReviewRepositoryCustom {
-	MatchReviewResponse.TotalCount getTeamTotalCount(Long teamId);
-
-	MatchReviewResponse.TotalCount getTeamTotalCountByUserId(Long userId);
-}
