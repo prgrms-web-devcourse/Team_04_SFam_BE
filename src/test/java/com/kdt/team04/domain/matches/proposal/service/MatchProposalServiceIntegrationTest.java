8bc544933b502c8e6fa1dce2ea67467ab863957d
HYEBPARK <hyeqls3742@gmail.com>
2022-07-30 01:28:38 +0900

    [DK-82] test: 매칭 신청 실패 케이스 추가

--- src/test/java/com/kdt/team04/domain/matches/proposal/service/MatchProposalServiceIntegrationTest.java
+++ src/test/java/com/kdt/team04/domain/matches/proposal/service/MatchProposalServiceIntegrationTest.java
@@ -1,6 +1,7 @@
-package com.kdt.team04.domain.matches.request.service;
+package com.kdt.team04.domain.matches.proposal.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 
@@ -12,14 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kdt.team04.common.exception.BusinessException;
 import com.kdt.team04.domain.matches.match.entity.Match;
 import com.kdt.team04.domain.matches.match.entity.MatchStatus;
 import com.kdt.team04.domain.matches.match.entity.MatchType;
-import com.kdt.team04.domain.matches.request.dto.MatchProposalRequest;
+import com.kdt.team04.domain.matches.proposal.dto.MatchProposalRequest;
 import com.kdt.team04.domain.team.SportsCategory;
 import com.kdt.team04.domain.team.entity.Team;
 import com.kdt.team04.domain.user.entity.User;
 
+@Transactional
 @SpringBootTest
 class MatchProposalServiceIntegrationTest {
 
@@ -30,7 +33,6 @@ class MatchProposalServiceIntegrationTest {
 	private MatchProposalService matchProposalService;
 
 	@Test
-	@Transactional
 	@DisplayName("개인전 매칭을 신청하고 해당 신청 생성 후 Id 값을 return 한다.")
 	void testIndividualCreateSuccess() {
 		//given
@@ -60,7 +62,6 @@ class MatchProposalServiceIntegrationTest {
 	}
 
 	@Test
-	@Transactional
 	@DisplayName("팀전 매칭을 신청하고 해당 신청 생성 후 Id 값을 return 한다.")
 	void testTeamProposerCreateSuccess() {
 		//given
@@ -87,7 +88,7 @@ class MatchProposalServiceIntegrationTest {
 			.title("match")
 			.status(MatchStatus.WAITING)
 			.matchDate(LocalDate.now())
-			.matchType(MatchType.INDIVIDUAL_MATCH)
+			.matchType(MatchType.TEAM_MATCH)
 			.participants(3)
 			.user(author)
 			.team(authorTeam)
@@ -95,12 +96,55 @@ class MatchProposalServiceIntegrationTest {
 			.content("content")
 			.build();
 		entityManager.persist(match);
-		MatchProposalRequest.ProposalCreate request = new MatchProposalRequest.ProposalCreate(proposerTeam.getId(), "팀전 신청합니다.");
+		MatchProposalRequest.ProposalCreate request = new MatchProposalRequest.ProposalCreate(proposerTeam.getId(),
+			"팀전 신청합니다.");
 
 		//when
 		Long createdProposer = matchProposalService.create(proposer.getId(), match.getId(), request);
 
 		//then
 		assertThat(createdProposer).isNotNull();
+	}
+
+	@Test
+	@DisplayName("팀전 매칭을 신청시 request의 teamId가 null일 경우 예외가 발생한다.")
+	void testTeamProposerCreateFail() {
+		//given
+		User author = new User("author", "author", "aA1234!");
+		User proposer = new User("proposer", "proposer", "aA1234!");
+		Team authorTeam = Team.builder()
+			.name("team1")
+			.description("first team")
+			.sportsCategory(SportsCategory.BADMINTON)
+			.leader(proposer)
+			.build();
+		Team proposerTeam = Team.builder()
+			.name("team1")
+			.description("first team")
+			.sportsCategory(SportsCategory.BADMINTON)
+			.leader(proposer)
+			.build();
+		entityManager.persist(authorTeam);
+		entityManager.persist(proposerTeam);
+		entityManager.persist(author);
+		entityManager.persist(proposer);
+
+		Match match = Match.builder()
+			.title("match")
+			.status(MatchStatus.WAITING)
+			.matchDate(LocalDate.now())
+			.matchType(MatchType.TEAM_MATCH)
+			.participants(3)
+			.user(author)
+			.team(authorTeam)
+			.sportsCategory(SportsCategory.BADMINTON)
+			.content("content")
+			.build();
+		entityManager.persist(match);
+		MatchProposalRequest.ProposalCreate request = new MatchProposalRequest.ProposalCreate(null, "팀전 신청합니다.");
+
+		//when, then
+		assertThatThrownBy(() -> matchProposalService.create(proposer.getId(), match.getId(), request))
+			.isInstanceOf(BusinessException.class);
 	}
 }
