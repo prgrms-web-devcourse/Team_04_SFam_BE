78130e3b8c2eb31b3737c1bf0da2bf80e206a948
킹받드라슈 <soccer_by@naver.com>
2022-08-04 01:43:22 +0900

    refactor: 필요없는 테스트 확인 코드 제거

--- src/test/java/com/kdt/team04/domain/user/service/UserServiceTest.java
+++ src/test/java/com/kdt/team04/domain/user/service/UserServiceTest.java
@@ -167,7 +167,6 @@ class UserServiceTest {
 		assertThat(userResponse.id()).isEqualTo(user.getId());
 		assertThat(userResponse.username()).isEqualTo(user.getUsername());
 		assertThat(userResponse.nickname()).isEqualTo(user.getNickname());
-		assertThat(userResponse.password()).isEqualTo(user.getPassword());
 	}
 
 	@Test
