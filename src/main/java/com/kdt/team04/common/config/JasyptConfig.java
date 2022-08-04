6030456bb19534820e40dad7f5bfe4e56a42e479
HyoungUkJJang <guddnr1451@gmail.com>
2022-08-05 04:27:50 +0900

    [DK-315] refactor : 로컬 프로파일 추가

--- src/main/java/com/kdt/team04/common/config/JasyptConfig.java
+++ src/main/java/com/kdt/team04/common/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"dev","real"})
+@Profile({"local", "dev", "real"})
 public class JasyptConfig {
 
 	@Value("${encryptor.key}")
--- src/main/java/com/kdt/team04/common/file/config/Aws.java
+++ src/main/java/com/kdt/team04/common/file/config/Aws.java
@@ -11,7 +11,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-@Profile({"dev", "real"})
+@Profile({"local", "dev", "real"})
 @Configuration
 @EnableConfigurationProperties({AwsConfigProperties.class, S3ConfigProperties.class})
 public class Aws {
