80863827c49065a777a98351cda7cc70d72a7dba
HyoungUkJJang <guddnr1451@gmail.com>
2022-08-05 03:13:30 +0900

    [DK-315] refactor : 팀 로고 이미지 업로드 코드 수정

--- src/main/java/com/kdt/team04/common/file/ImagePath.java
+++ src/main/java/com/kdt/team04/common/file/ImagePath.java
@@ -1,7 +1,5 @@
 package com.kdt.team04.common.file;
 
-// https://sfam-bucket.s3.ap-northeast-2.amazonaws.com - prefix
-// users/profile/11f55110-9732-483a-b10a-bb7bcb7ba728.jpeg - key
 public enum ImagePath {
 	USERS_PROFILES("users/profile/"),
 	TEAMS_LOGO("teams/logo/");
