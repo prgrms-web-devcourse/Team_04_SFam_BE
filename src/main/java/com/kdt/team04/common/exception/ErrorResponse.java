8920c2e86f2add08ee979c499e5796249fdecf20
pjh612 <pjh_jn@naver.com>
2022-08-09 20:34:15 +0900

    feat: ErrorResponse에 에러 시간 포함

--- src/main/java/com/kdt/team04/common/exception/ErrorResponse.java
+++ src/main/java/com/kdt/team04/common/exception/ErrorResponse.java
@@ -1,12 +1,16 @@
 package com.kdt.team04.common.exception;
 
+import java.time.LocalDateTime;
+
 public class ErrorResponse<T extends ErrorCode> {
 	private final String code;
 	private final String message;
+	private final LocalDateTime dateTime;
 
 	public ErrorResponse(T errorCode) {
 		this.code = errorCode.getCode();
 		this.message = errorCode.getMessage();
+		this.dateTime = LocalDateTime.now();
 	}
 
 	public String getCode() {
@@ -15,5 +19,9 @@ public class ErrorResponse<T extends ErrorCode> {
 
 	public String getMessage() {
 		return message;
+	}
+
+	public LocalDateTime dateTime() {
+		return dateTime;
 	}
 }
