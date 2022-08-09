8713d21585140322509fd3f55090d67ab9d10754
HyoungUkJJang <guddnr1451@gmail.com>
2022-08-10 03:50:31 +0900

    [DK-415] refactor : 팀 초대 중복 안되도록 메서드 추가

--- src/main/java/com/kdt/team04/domain/teams/teaminvitation/repository/TeamInvitationRepository.java
+++ src/main/java/com/kdt/team04/domain/teams/teaminvitation/repository/TeamInvitationRepository.java
@@ -1,5 +1,6 @@
 package com.kdt.team04.domain.teams.teaminvitation.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +11,9 @@ import com.kdt.team04.domain.teams.teaminvitation.model.entity.TeamInvitation;
 public interface TeamInvitationRepository extends JpaRepository<TeamInvitation, Long>, CustomTeamInvitationRepository {
 	boolean existsByTeamIdAndTargetIdAndStatus(Long teamId, Long targetId, InvitationStatus status);
 
+	boolean existsByTeamIdAndTargetIdAndStatusIn(Long teamId, Long targetId, List<InvitationStatus> statusList);
+
 	Optional<TeamInvitation> findByTeamIdAndTargetId(Long teamId, Long targetId);
 
 	Optional<TeamInvitation> findByIdAndTeamId(Long id, Long teamId);
-
 }
