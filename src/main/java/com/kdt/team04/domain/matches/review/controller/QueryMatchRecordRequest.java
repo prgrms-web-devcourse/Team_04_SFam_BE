adf8ce0c781c5334605f93ec96241278d33b9d52
pjh612 <pjh_jn@naver.com>
2022-08-12 17:59:03 +0900

    fix: 쿼리 파라미터이므로 스웨거 명세 @Parameter로 변경

--- src/main/java/com/kdt/team04/domain/matches/review/controller/QueryMatchRecordRequest.java
+++ src/main/java/com/kdt/team04/domain/matches/review/controller/QueryMatchRecordRequest.java
@@ -4,16 +4,16 @@ import javax.validation.constraints.AssertTrue;
 
 import com.kdt.team04.domain.teams.team.model.SportsCategory;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Parameter;
 
 public record QueryMatchRecordRequest(
-	@Schema(description = "스포츠 종목(값/설명) - BADMINTON/배드민턴, SOCCER/축구, BASEBALL/야구")
+	@Parameter(description = "스포츠 종목(값/설명) - BADMINTON/배드민턴, SOCCER/축구, BASEBALL/야구")
 	SportsCategory sportsCategory,
 
-	@Schema(description = "회원 ID(고유 PK)")
+	@Parameter(description = "회원 ID(고유 PK)")
 	Long userId,
 
-	@Schema(description = "팀 ID(고유 PK)")
+	@Parameter(description = "팀 ID(고유 PK)")
 	Long teamId
 ) {
 
