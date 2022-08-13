dba4013f60eae14b46fc7625fd982d96a9f16581
pjh612 <pjh_jn@naver.com>
2022-08-14 03:43:35 +0900

    feat: #87 경기 종목 추가

--- src/main/java/com/kdt/team04/domain/teams/team/model/SportsCategory.java
+++ src/main/java/com/kdt/team04/domain/teams/team/model/SportsCategory.java
@@ -3,7 +3,16 @@ package com.kdt.team04.domain.teams.team.model;
 public enum SportsCategory {
 	BADMINTON("배드민턴"),
 	SOCCER("축구"),
-	BASEBALL("야구");
+	BASEBALL("야구"),
+	BASKETBALL("농구"),
+	TENNIS("테니스"),
+	GOLF("골프"),
+	BILLIARD("당구"),
+	JOKGU("족구"),
+	BOWLING("볼링"),
+	ARM_WRESTLING("팔씨름"),
+	PING_PONG("탁구");
+
 
 	private final String name;
 
