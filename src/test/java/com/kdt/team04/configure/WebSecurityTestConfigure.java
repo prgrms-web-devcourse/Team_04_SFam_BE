0e32e1e387aec878f694bab5d44f12660439db49
pjh612 <pjh_jn@naver.com>
2022-08-14 00:46:12 +0900

    test: #87 kakao service 모킹

--- src/test/java/com/kdt/team04/configure/WebSecurityTestConfigure.java
+++ src/test/java/com/kdt/team04/configure/WebSecurityTestConfigure.java
@@ -4,7 +4,6 @@ package com.kdt.team04.configure;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
-import com.kdt.team04.common.config.CorsConfigProperties;
 import com.kdt.team04.common.security.SecurityConfigProperties;
 import com.kdt.team04.common.security.WebSecurityConfig;
 import com.kdt.team04.domain.auth.service.JpaTokenService;
